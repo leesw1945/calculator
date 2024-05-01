f3ff657423422d17f86c31aa59a5dfe8ab115ec9
Robbie <robbie331@>
2024-05-01 10:17:26 +0900

    level1-4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,31 +7,40 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
-
         int result = 0;
-        switch (operator){
-            case '+':
-                result = num1 + num2;
+        while (true){
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            switch (operator){
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                default:
+                    System.out.println("잘 못 입력하였습니다.");
+                    continue;
+            }
+            System.out.println("결과: " + result);
+            String lineFeed = sc.nextLine();
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.nextLine();
+            if (exit.equals("exit")){
                 break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '/':
-                result = num1 / num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            default:
-                System.out.println("잘 못 입력하였습니다.");
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
