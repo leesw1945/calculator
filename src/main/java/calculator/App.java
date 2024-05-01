f445a467f55c3a24fe30d305b3f14338fd9c6b4c
Robbie <robbie331@>
2024-05-01 18:27:59 +0900

    level2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,14 +6,12 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-
-        ArrayList<Integer> resultArrList = new ArrayList<>();
+        Calculator calculator = new Calculator();
 
         Scanner sc = new Scanner(System.in);
 
-        int result = 0;
         while (true){
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
@@ -23,38 +21,28 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
-            switch (operator){
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                default:
-                    System.out.println("잘 못 입력하였습니다.");
-                    continue;
+            try{
+                calculator.calculate(num1, num2, operator);
             }
-            System.out.println("결과: " + result);
+            catch (BadException e){
+                System.out.println(e.getMessage());
+            }
+            finally {
+                System.out.println("예외처리");
+            }
 
-            // ArrayList 1-7
-            resultArrList.add(result);
-
+            System.out.println("결과: " + calculator.result);
             String lineFeed = sc.nextLine();
+
             System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String remove = sc.nextLine();
             if (remove.equals("remove")){
-                resultArrList.remove(0);
+                calculator.resultArrList.remove(0);
             }
             System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String inquiry = sc.nextLine();
             if (inquiry.equals("inquiry")){
-                for (int select : resultArrList){
+                for (int select : calculator.resultArrList){
                     System.out.println(select);
                 }
             }
@@ -64,6 +52,6 @@ public class App {
                 break;
             }
         }
-        System.out.println(resultArrList.toString());
+        System.out.println(calculator.resultArrList.toString());
     }
 }
